ca3399d9aa33e191d3ceab9aa4e74f3700d87d13
naveenjaiswal123 <60127870+naveenjaiswal123@users.noreply.github.com>
2021-10-19 10:58:59 +0530

    Add files via upload

---
+++ App_Code/DBConnection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for DBConnection
+/// </summary>
+public class DBConnection
+{
+
+
+
+    public String ConString = "Data Source=117.240.18.180;Initial Catalog=SchoolDB;User ID=sa; password=Ph@hoenix#g";
+    public DBConnection()
+    {
+        
+    }
+}
---
+++ App_Code/Global.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Fees
+/// </summary>
+public class Global
+{
+    public static String ReceiptNo_Global;
+    public static String TransType_Global;
+
+
+}
